a490bd49979854a333a58a440d55bba999b82c7d
Tomasz Turkiewicz <tturkiewicz83@gmail.com>
2019-09-22 15:11:35 +0100

    creating battle field layout

--- app/src/main/java/com/example/ships/CreateBattleField.java
+++ app/src/main/java/com/example/ships/CreateBattleField.java
@@ -2,6 +2,7 @@ package com.example.ships;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateBattleField extends AppCompatActivity {
 
@@ -9,5 +10,323 @@ public class CreateBattleField extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_battle_field);
+    }
+
+    public void onClickBack(View view) {
+    }
+
+    public void onClickStartGame(View view) {
+    }
+
+    public void onClickputOneMastsShip(View view) {
+    }
+
+    public void onClickputTwoMastsShip(View view) {
+    }
+
+    public void onClickputThreeMastsShip(View view) {
+    }
+
+    public void onClickputFourMastsShip(View view) {
+    }
+
+    public void clickCreateBattleField_1x1(View view) {
+    }
+
+    public void clickCreateBattleField_1x2(View view) {
+    }
+
+    public void clickCreateBattleField_1x3(View view) {
+    }
+
+    public void clickCreateBattleField_1x4(View view) {
+    }
+
+    public void clickCreateBattleField_1x5(View view) {
+    }
+
+    public void clickCreateBattleField_1x6(View view) {
+    }
+
+    public void clickCreateBattleField_1x7(View view) {
+    }
+
+    public void clickCreateBattleField_1x8(View view) {
+    }
+
+    public void clickCreateBattleField_1x9(View view) {
+    }
+
+    public void clickCreateBattleField_1x10(View view) {
+    }
+
+    public void clickCreateBattleField_2x1(View view) {
+    }
+
+    public void clickCreateBattleField_2x2(View view) {
+    }
+
+    public void clickCreateBattleField_2x3(View view) {
+    }
+
+    public void clickCreateBattleField_2x4(View view) {
+    }
+
+    public void clickCreateBattleField_2x5(View view) {
+    }
+
+    public void clickCreateBattleField_2x6(View view) {
+    }
+
+    public void clickCreateBattleField_2x7(View view) {
+    }
+
+    public void clickCreateBattleField_2x8(View view) {
+    }
+
+    public void clickCreateBattleField_2x9(View view) {
+    }
+
+    public void clickCreateBattleField_2x10(View view) {
+    }
+
+    public void clickCreateBattleField_3x1(View view) {
+    }
+
+    public void clickCreateBattleField_3x2(View view) {
+    }
+
+    public void clickCreateBattleField_3x3(View view) {
+    }
+
+    public void clickCreateBattleField_3x4(View view) {
+    }
+
+    public void clickCreateBattleField_3x5(View view) {
+    }
+
+    public void clickCreateBattleField_3x6(View view) {
+    }
+
+    public void clickCreateBattleField_3x7(View view) {
+    }
+
+    public void clickCreateBattleField_3x8(View view) {
+    }
+
+    public void clickCreateBattleField_3x9(View view) {
+    }
+
+    public void clickCreateBattleField_3x10(View view) {
+    }
+
+    public void clickCreateBattleField_4x1(View view) {
+    }
+
+    public void clickCreateBattleField_4x2(View view) {
+    }
+
+    public void clickCreateBattleField_4x3(View view) {
+    }
+
+    public void clickCreateBattleField_4x4(View view) {
+    }
+
+    public void clickCreateBattleField_4x5(View view) {
+    }
+
+    public void clickCreateBattleField_4x6(View view) {
+    }
+
+    public void clickCreateBattleField_4x7(View view) {
+    }
+
+    public void clickCreateBattleField_4x8(View view) {
+    }
+
+    public void clickCreateBattleField_4x9(View view) {
+    }
+
+    public void clickCreateBattleField_4x10(View view) {
+    }
+
+    public void clickCreateBattleField_5x1(View view) {
+    }
+
+    public void clickCreateBattleField_5x2(View view) {
+    }
+
+    public void clickCreateBattleField_5x3(View view) {
+    }
+
+    public void clickCreateBattleField_5x4(View view) {
+    }
+
+    public void clickCreateBattleField_5x5(View view) {
+    }
+
+    public void clickCreateBattleField_5x6(View view) {
+    }
+
+    public void clickCreateBattleField_5x7(View view) {
+    }
+
+    public void clickCreateBattleField_5x8(View view) {
+    }
+
+    public void clickCreateBattleField_5x9(View view) {
+    }
+
+    public void clickCreateBattleField_5x10(View view) {
+    }
+
+    public void clickCreateBattleField_6x1(View view) {
+    }
+
+    public void clickCreateBattleField_6x2(View view) {
+    }
+
+    public void clickCreateBattleField_6x3(View view) {
+    }
+
+    public void clickCreateBattleField_6x4(View view) {
+    }
+
+    public void clickCreateBattleField_6x5(View view) {
+    }
+
+    public void clickCreateBattleField_6x6(View view) {
+    }
+
+    public void clickCreateBattleField_6x7(View view) {
+    }
+
+    public void clickCreateBattleField_6x8(View view) {
+    }
+
+    public void clickCreateBattleField_6x9(View view) {
+    }
+
+    public void clickCreateBattleField_6x10(View view) {
+    }
+
+    public void clickCreateBattleField_7x1(View view) {
+    }
+
+    public void clickCreateBattleField_7x2(View view) {
+    }
+
+    public void clickCreateBattleField_7x3(View view) {
+    }
+
+    public void clickCreateBattleField_7x4(View view) {
+    }
+
+    public void clickCreateBattleField_7x5(View view) {
+    }
+
+    public void clickCreateBattleField_7x6(View view) {
+    }
+
+    public void clickCreateBattleField_7x7(View view) {
+    }
+
+    public void clickCreateBattleField_7x8(View view) {
+    }
+
+    public void clickCreateBattleField_7x9(View view) {
+    }
+
+    public void clickCreateBattleField_7x10(View view) {
+    }
+
+    public void clickCreateBattleField_8x1(View view) {
+    }
+
+    public void clickCreateBattleField_8x2(View view) {
+    }
+
+    public void clickCreateBattleField_8x3(View view) {
+    }
+
+    public void clickCreateBattleField_8x4(View view) {
+    }
+
+    public void clickCreateBattleField_8x5(View view) {
+    }
+
+    public void clickCreateBattleField_8x6(View view) {
+    }
+
+    public void clickCreateBattleField_8x7(View view) {
+    }
+
+    public void clickCreateBattleField_8x8(View view) {
+    }
+
+    public void clickCreateBattleField_8x9(View view) {
+    }
+
+    public void clickCreateBattleField_8x10(View view) {
+    }
+
+    public void clickCreateBattleField_9x1(View view) {
+    }
+
+    public void clickCreateBattleField_9x2(View view) {
+    }
+
+    public void clickCreateBattleField_9x3(View view) {
+    }
+
+    public void clickCreateBattleField_9x4(View view) {
+    }
+
+    public void clickCreateBattleField_9x5(View view) {
+    }
+
+    public void clickCreateBattleField_9x6(View view) {
+    }
+
+    public void clickCreateBattleField_9x7(View view) {
+    }
+
+    public void clickCreateBattleField_9x8(View view) {
+    }
+
+    public void clickCreateBattleField_9x9(View view) {
+    }
+
+    public void clickCreateBattleField_9x10(View view) {
+    }
+
+    public void clickCreateBattleField_10x1(View view) {
+    }
+
+    public void clickCreateBattleField_10x2(View view) {
+    }
+
+    public void clickCreateBattleField_10x3(View view) {
+    }
+
+    public void clickCreateBattleField_10x4(View view) {
+    }
+
+    public void clickCreateBattleField_10x5(View view) {
+    }
+
+    public void clickCreateBattleField_10x6(View view) {
+    }
+
+    public void clickCreateBattleField_10x7(View view) {
+    }
+
+    public void clickCreateBattleField_10x8(View view) {
+    }
+
+    public void clickCreateBattleField_10x9(View view) {
+    }
+
+    public void clickCreateBattleField_10x10(View view) {
     }
 }
