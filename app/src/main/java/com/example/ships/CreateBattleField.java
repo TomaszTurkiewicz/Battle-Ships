3b31b59a1507ccb197379931e0d93657daaa3a12
Tomasz Turkiewicz <tturkiewicz83@gmail.com>
2019-09-25 22:24:24 +0100

    creating battle field (counting ships on battle field)

--- app/src/main/java/com/example/ships/CreateBattleField.java
+++ app/src/main/java/com/example/ships/CreateBattleField.java
@@ -10,6 +10,30 @@ public class CreateBattleField extends AppCompatActivity {
 
     BattleField battleFieldPlayerCreateBattleFieldActivity = new BattleField();
     TextView[][] TextViewArrayActivityCreateBattleField = new TextView[10][10];
+    TextView[] FourMastsShip = new TextView[4];
+    TextView[] ThreeMastsShip = new TextView[3];
+    TextView[] TwoMastsShip = new TextView[2];
+    TextView[] OneMastsShip = new TextView[1];
+    TextView FourMastsCounter;
+    TextView ThreeMastsCounter;
+    TextView TwoMastsCounter;
+    TextView OneMastsCounter;
+    private int fourMastsCounter;
+    private int threeMastsCounter1;
+    private int threeMastsCounter2;
+    private int twoMastsCounter1;
+    private int twoMastsCounter2;
+    private int twoMastsCounter3;
+    private int oneMastsCounter1;
+    private int oneMastsCounter2;
+    private int oneMastsCounter3;
+    private int oneMastsCounter4;
+    int leftFourMasts;
+    int leftThreeMasts;
+    int leftTwoMasts;
+    int leftOneMasts;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +41,128 @@ public class CreateBattleField extends AppCompatActivity {
         setContentView(R.layout.activity_create_battle_field);
 
         initializeBattleFieldActivityRandomGamePlayerOne(TextViewArrayActivityCreateBattleField);
+        FourMastsCounter = (TextView)findViewById(R.id.fourMastsCounterTextView);
+        ThreeMastsCounter = (TextView)findViewById(R.id.threeMastsCounterTextView);
+        TwoMastsCounter = (TextView)findViewById(R.id.twoMastsCounterTextView);
+        OneMastsCounter = (TextView)findViewById(R.id.oneMastsCounterTextView);
+        initializeFourMastsShipTextView(FourMastsShip);
+        initializeThreeMastsShipTextView(ThreeMastsShip);
+        initializeTwoMastsShipTextView(TwoMastsShip);
+        initializeOneMastsShipTextView(OneMastsShip);
+
+//
+//        battleFieldPlayerCreateBattleFieldActivity.battleField[0][0].setShip(true);
+//        battleFieldPlayerCreateBattleFieldActivity.battleField[0][0].setNumberOfMasts(1);
+//        battleFieldPlayerCreateBattleFieldActivity.battleField[0][0].setShipNumber(1);
+//
+//        battleFieldPlayerCreateBattleFieldActivity.battleField[0][3].setShip(true);
+//        battleFieldPlayerCreateBattleFieldActivity.battleField[0][3].setNumberOfMasts(1);
+//        battleFieldPlayerCreateBattleFieldActivity.battleField[0][3].setShipNumber(3);
+//
+//        battleFieldPlayerCreateBattleFieldActivity.battleField[1][3].setShip(true);
+//        battleFieldPlayerCreateBattleFieldActivity.battleField[1][3].setNumberOfMasts(1);
+//        battleFieldPlayerCreateBattleFieldActivity.battleField[1][3].setShipNumber(2);
+
+        checkShipsOnBattleField(battleFieldPlayerCreateBattleFieldActivity);
+        checkLeftShips();
+        updateTextViewCounters();
+
+    }
+
+    private void checkLeftShips() {
+        leftFourMasts=1-(fourMastsShipBoolean() ? 1 : 0);
+        leftThreeMasts=2-((threeMastsShip1Boolean()?1:0)+(threeMastsShip2Boolean()?1:0));
+        leftTwoMasts=3-((twoMastsShip1Boolean()?1:0)+(twoMastsShip2Boolean()?1:0)+(twoMastsShip3Boolean()?1:0));
+        leftOneMasts=4-((oneMastsShip1Boolean()?1:0)+(oneMastsShip2Boolean()?1:0)+(oneMastsShip3Boolean()?1:0)+(oneMastsShip4Boolean()?1:0));
+    }
+
+    private void updateTextViewCounters() {
+        FourMastsCounter.setText(String.valueOf(leftFourMasts));
+        ThreeMastsCounter.setText(String.valueOf(leftThreeMasts));
+        TwoMastsCounter.setText(String.valueOf(leftTwoMasts));
+        OneMastsCounter.setText(String.valueOf(leftOneMasts));
+
+    }
+
+
+    private void checkShipsOnBattleField(BattleField battleFieldPlayerCreateBattleFieldActivity) {
+        fourMastsCounter=0;
+        threeMastsCounter1=0;
+        threeMastsCounter2=0;
+        twoMastsCounter1=0;
+        twoMastsCounter2=0;
+        twoMastsCounter3=0;
+        oneMastsCounter1=0;
+        oneMastsCounter2=0;
+        oneMastsCounter3=0;
+        oneMastsCounter4=0;
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++){
+                if(battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].isShip()){
+                    if(battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].getNumberOfMasts()==4){
+                        fourMastsCounter++;
+                    }
+                    else if(battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].getNumberOfMasts()==3&&
+                            battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].getShipNumber()==1){
+                        threeMastsCounter1++;
+                    }
+                    else if(battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].getNumberOfMasts()==3&&
+                            battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].getShipNumber()==2){
+                        threeMastsCounter2++;
+                    }
+                    else if(battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].getNumberOfMasts()==2&&
+                            battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].getShipNumber()==1){
+                        twoMastsCounter1++;
+                    }
+                    else if(battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].getNumberOfMasts()==2&&
+                            battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].getShipNumber()==2){
+                        twoMastsCounter2++;
+                    }
+                    else if(battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].getNumberOfMasts()==2&&
+                            battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].getShipNumber()==3){
+                        twoMastsCounter3++;
+                    }
+                    else if(battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].getNumberOfMasts()==1&&
+                            battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].getShipNumber()==1){
+                        oneMastsCounter1++;
+                    }
+                    else if(battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].getNumberOfMasts()==1&&
+                            battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].getShipNumber()==2){
+                        oneMastsCounter2++;
+                    }
+                    else if(battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].getNumberOfMasts()==1&&
+                            battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].getShipNumber()==3){
+                        oneMastsCounter3++;
+                    }
+                    else if(battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].getNumberOfMasts()==1&&
+                            battleFieldPlayerCreateBattleFieldActivity.battleField[i][j].getShipNumber()==4){
+                        oneMastsCounter4++;
+                    }else;
+                }else;
+            }
+        }
+    }
+
+    private void initializeOneMastsShipTextView(TextView[] oneMastsShip) {
+        oneMastsShip[0]=(TextView)findViewById(R.id.playerCellGameCreateBattleFieldOneMasts1);
+    }
+
+    private void initializeTwoMastsShipTextView(TextView[] twoMastsShip) {
+        twoMastsShip[0]=(TextView)findViewById(R.id.playerCellGameCreateBattleFieldTwoMasts1);
+        twoMastsShip[1]=(TextView)findViewById(R.id.playerCellGameCreateBattleFieldTwoMasts2);
+    }
+
+    private void initializeThreeMastsShipTextView(TextView[] threeMastsShip) {
+        threeMastsShip[0]=(TextView)findViewById(R.id.playerCellGameCreateBattleFieldThreeMasts1);
+        threeMastsShip[1]=(TextView)findViewById(R.id.playerCellGameCreateBattleFieldThreeMasts2);
+        threeMastsShip[2]=(TextView)findViewById(R.id.playerCellGameCreateBattleFieldThreeMasts3);
+    }
+
+    private void initializeFourMastsShipTextView(TextView[] fourMastsShip) {
+        fourMastsShip[0]=(TextView)findViewById(R.id.playerCellGameCreateBattleFieldFourMasts1);
+        fourMastsShip[1]=(TextView)findViewById(R.id.playerCellGameCreateBattleFieldFourMasts2);
+        fourMastsShip[2]=(TextView)findViewById(R.id.playerCellGameCreateBattleFieldFourMasts3);
+        fourMastsShip[3]=(TextView)findViewById(R.id.playerCellGameCreateBattleFieldFourMasts4);
     }
 
     private void initializeBattleFieldActivityRandomGamePlayerOne(TextView[][] textViewArrayActivityCreateBattleField) {
@@ -108,7 +254,27 @@ public class CreateBattleField extends AppCompatActivity {
         textViewArrayActivityCreateBattleField[7][8]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_8x9);
         textViewArrayActivityCreateBattleField[7][9]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_8x10);
 
-        //todo dokończyć inicjalizację tablicy
+        textViewArrayActivityCreateBattleField[8][0]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_9x1);
+        textViewArrayActivityCreateBattleField[8][1]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_9x2);
+        textViewArrayActivityCreateBattleField[8][2]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_9x3);
+        textViewArrayActivityCreateBattleField[8][3]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_9x4);
+        textViewArrayActivityCreateBattleField[8][4]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_9x5);
+        textViewArrayActivityCreateBattleField[8][5]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_9x6);
+        textViewArrayActivityCreateBattleField[8][6]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_9x7);
+        textViewArrayActivityCreateBattleField[8][7]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_9x8);
+        textViewArrayActivityCreateBattleField[8][8]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_9x9);
+        textViewArrayActivityCreateBattleField[8][9]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_9x10);
+
+        textViewArrayActivityCreateBattleField[9][0]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_10x1);
+        textViewArrayActivityCreateBattleField[9][1]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_10x2);
+        textViewArrayActivityCreateBattleField[9][2]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_10x3);
+        textViewArrayActivityCreateBattleField[9][3]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_10x4);
+        textViewArrayActivityCreateBattleField[9][4]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_10x5);
+        textViewArrayActivityCreateBattleField[9][5]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_10x6);
+        textViewArrayActivityCreateBattleField[9][6]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_10x7);
+        textViewArrayActivityCreateBattleField[9][7]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_10x8);
+        textViewArrayActivityCreateBattleField[9][8]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_10x9);
+        textViewArrayActivityCreateBattleField[9][9]=(TextView)findViewById(R.id.playerCellGameCreateBattleField_10x10);
     }
 
     public void onClickBack(View view) {
@@ -117,6 +283,7 @@ public class CreateBattleField extends AppCompatActivity {
     public void onClickStartGame(View view) {
         Intent intent = new Intent(getApplicationContext(),ChooseGameLevel.class);
         startActivity(intent);
+        finish();
     }
 
     public void onClickputOneMastsShip(View view) {
@@ -129,6 +296,7 @@ public class CreateBattleField extends AppCompatActivity {
     }
 
     public void onClickputFourMastsShip(View view) {
+
     }
 
     public void clickCreateBattleField_1x1(View view) {
@@ -429,5 +597,35 @@ public class CreateBattleField extends AppCompatActivity {
     }
 
     public void clickCreateBattleField_10x10(View view) {
+    }
+    private boolean fourMastsShipBoolean(){
+        return fourMastsCounter==4;
+    }
+    private boolean threeMastsShip1Boolean(){
+        return threeMastsCounter1==3;
+    }
+    private boolean threeMastsShip2Boolean(){
+        return threeMastsCounter2==3;
+    }
+    private boolean twoMastsShip1Boolean(){
+        return twoMastsCounter1==2;
+    }
+    private boolean twoMastsShip2Boolean(){
+        return twoMastsCounter2==2;
+    }
+    private boolean twoMastsShip3Boolean(){
+        return twoMastsCounter3==2;
+    }
+    private boolean oneMastsShip1Boolean(){
+        return oneMastsCounter1==1;
+    }
+    private boolean oneMastsShip2Boolean(){
+        return oneMastsCounter2==1;
+    }
+    private boolean oneMastsShip3Boolean(){
+        return oneMastsCounter3==1;
+    }
+    private boolean oneMastsShip4Boolean(){
+        return oneMastsCounter4==1;
     }
 }
