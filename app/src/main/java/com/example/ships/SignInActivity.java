1747bb3068b4b9322ce255ea2ee7952c729f6279
Tomasz Turkiewicz <tturkiewicz83@gmail.com>
2020-01-03 22:19:01 +0000

    google login button cdn...

--- app/src/main/java/com/example/ships/SignInActivity.java
+++ app/src/main/java/com/example/ships/SignInActivity.java
@@ -1,20 +1,26 @@
 package com.example.ships;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -29,6 +35,8 @@ public class SignInActivity extends AppCompatActivity {
     static final int GOOGLE_SIGN = 123;
     private Button login_google;
     //TODO dokończyć logowanie przez googla
+    private GoogleSignInClient mGoogleSignInClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +52,16 @@ public class SignInActivity extends AppCompatActivity {
 
         loggedIn = firebaseAuth.getCurrentUser()!= null&&firebaseAuth.getCurrentUser().isEmailVerified();
 
+        login_google = findViewById(R.id.loginGoogle);
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions
+                .Builder()
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+
+        
 
 
         if(!loggedIn){
@@ -53,64 +71,94 @@ public class SignInActivity extends AppCompatActivity {
             userID = firebaseUser.getUid();
         }
 
-        deleteUser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        deleteUser.setOnClickListener(v -> {
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(SignInActivity.this);
-                builder.setTitle("DELETE ACCOUNT");
-                builder.setMessage("Do you really want to delete your account?");
-                builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        databaseReference.child(userID).removeValue();
-                        firebaseUser.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if(task.isSuccessful()){
-                                    Toast.makeText(SignInActivity.this,"Account Deleted",Toast.LENGTH_LONG).show();
-                                    Intent intent = new Intent(SignInActivity.this, MainActivity.class);
-                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                    startActivity(intent);
-                                    finish();
-                                }
-                            }
-                        });
-
+            AlertDialog.Builder builder = new AlertDialog.Builder(SignInActivity.this);
+            builder.setTitle("DELETE ACCOUNT");
+            builder.setMessage("Do you really want to delete your account?");
+            builder.setPositiveButton("YES", (dialog, which) -> {
+                dialog.dismiss();
+                databaseReference.child(userID).removeValue();
+                firebaseUser.delete().addOnCompleteListener(task -> {
+                    if(task.isSuccessful()){
+                        Toast.makeText(SignInActivity.this,"Account Deleted",Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        finish();
                     }
                 });
-                builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
+
+            });
+            builder.setNegativeButton("NO", (dialog, which) -> dialog.dismiss());
+            AlertDialog alert = builder.create();
+            alert.show();
+
+
+
+
+
+
+
+
                 });
-                AlertDialog alert = builder.create();
-                alert.show();
 
-
-
-
-
-
-
-
-                    }
+        userLogout.setOnClickListener(view -> {
+            firebaseAuth.signOut();
+            Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
         });
+    }
 
-        userLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                firebaseAuth.signOut();
-                Intent intent = new Intent(SignInActivity.this, MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
-                finish();
+
+    void SignInGoogle(){
+        Intent signIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signIntent,GOOGLE_SIGN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==GOOGLE_SIGN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try{
+
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                if(account!=null) firebaseAuthWithGoogle(account);
+
+            }catch (ApiException e){
+                e.printStackTrace();
             }
-        });
+
+
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        Log.d("TAG", "firebaseAuthWithGoogle: "+account.getId());
+
+        AuthCredential credential = GoogleAuthProvider
+                .getCredential(account.getIdToken(),null);
+
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+
+                    if(task.isSuccessful()){
+                        Log.d("TAG","signin success");
+                        Toast.makeText(this, "Signin success", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Log.d("TAG","signin failure", task.getException());
+                        Toast.makeText(this, "Signin Failed", Toast.LENGTH_SHORT).show();
+                    }
+
+
+                });
+
     }
 
     public void onClickEmailAndPasswordLogin(View view) {
